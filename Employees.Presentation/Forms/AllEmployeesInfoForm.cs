ba4a3b980b769bb1042297e8d4d83660180de53e
AAmanzi <alnick.amanzi@gmail.com>
2019-01-11 08:41:22 +0100

    Refactored AllEmployeesInfoForm

--- Employees.Presentation/Forms/AllEmployeesInfoForm.cs
+++ Employees.Presentation/Forms/AllEmployeesInfoForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Employees.Data.Models;
 using Employees.Domain.Repositories;
 
 namespace Employees.Presentation.Forms
@@ -16,56 +17,77 @@ namespace Employees.Presentation.Forms
         private void RefreshEmployeeInfo()
         {
             EmployeeInfoListView.Items.Clear();
-
-            EmployeeInfoListView.Columns.Add("Name", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Last name", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Date of birth", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("OIB", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Position", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Work hours this week", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Ended projects", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Active projects", -2, HorizontalAlignment.Left);
-            EmployeeInfoListView.Columns.Add("Upcoming projects", -2, HorizontalAlignment.Left);
+            AddEmployeeColumns(EmployeeInfoListView);
 
             foreach (var employee in EmployeeRepo.GetEmployees())
             {
                 var employeeItem = new ListViewItem(employee.Name);
+                SetEmployeeBackColor(employeeItem, employee);
+                AddEmployeeSubItems(employeeItem, employee);
+                SetEmployeeProjects(employeeItem, employee);
 
-                if (RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(employee.Oib) > 40)
-                    employeeItem.BackColor = Color.DarkSlateGray;
-                else if(RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(employee.Oib) < 30)
-                    employeeItem.BackColor = Color.Goldenrod;
-                else
-                    employeeItem.BackColor = Color.Aquamarine;
 
-                employeeItem.SubItems.Add(employee.LastName);
-                employeeItem.SubItems.Add($"{employee.DateOfBirth:d}");
-                employeeItem.SubItems.Add(employee.Oib);
-                employeeItem.SubItems.Add(employee.Position.ToString());
-                employeeItem.SubItems.Add(RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(employee.Oib).ToString());
 
-                var projectsEnded = 0;
-                var projectsActive = 0;
-                var projectsUpcoming = 0;
-                foreach (var project in RelationProjectEmployeeRepo.GetProjectsByEmployee(employee.Oib))
-                {
-                    if (project.HasEnded())
-                        projectsEnded++;
-                    if (project.HasStarted() && !project.HasEnded())
-                        projectsActive++;
-                    if (!project.HasStarted())
-                        projectsUpcoming++;
-                }
-
-                employeeItem.SubItems.Add(projectsEnded.ToString());
-                employeeItem.SubItems.Add(projectsActive.ToString());
-                employeeItem.SubItems.Add(projectsUpcoming.ToString());
 
                 EmployeeInfoListView.Items.Add(employeeItem);
             }
             
         }
 
+        private void AddEmployeeColumns(ListView destination)
+        {
+            destination.Columns.Add("Name", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Last name", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Date of birth", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("OIB", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Position", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Work hours this week", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Ended projects", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Active projects", -2, HorizontalAlignment.Left);
+            destination.Columns.Add("Upcoming projects", -2, HorizontalAlignment.Left);
+        }
+
+        private void AddEmployeeSubItems(ListViewItem destination, Employee source)
+        {
+            destination.SubItems.Add(source.LastName);
+            destination.SubItems.Add($"{source.DateOfBirth:d}");
+            destination.SubItems.Add(source.Oib);
+            destination.SubItems.Add(source.Position.ToString());
+            destination.SubItems.Add(RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(source.Oib).ToString());
+        }
+
+        private static void SetEmployeeBackColor(ListViewItem destination, Employee hoursSource)
+        {
+
+            if (RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(hoursSource.Oib) > 40)
+                destination.BackColor = Color.Red;
+            else if (RelationProjectEmployeeRepo.EmployeeThisWeeksWorkHours(hoursSource.Oib) < 30)
+                destination.BackColor = Color.Goldenrod;
+            else
+                destination.BackColor = Color.MediumSeaGreen;
+        }
+
+        private static void SetEmployeeProjects(ListViewItem destination, Employee projectSource)
+        {
+            var projectsEnded = 0;
+            var projectsActive = 0;
+            var projectsUpcoming = 0;
+            foreach (var project in RelationProjectEmployeeRepo.GetProjectsByEmployee(projectSource.Oib))
+            {
+                if (project.HasEnded())
+                    projectsEnded++;
+                if (project.HasStarted() && !project.HasEnded())
+                    projectsActive++;
+                if (!project.HasStarted())
+                    projectsUpcoming++;
+            }
+
+            destination.SubItems.Add(projectsEnded.ToString());
+            destination.SubItems.Add(projectsActive.ToString());
+            destination.SubItems.Add(projectsUpcoming.ToString());
+        }
+
+        //buttons
         private void ManageButton_Click(object sender, EventArgs e)
         {
             var manageEmployees = new ManageEmployeesForm();
